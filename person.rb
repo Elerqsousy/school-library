a27b99df5a1d70049217ff09e8f0f79a52712d84
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-12-22 15:21:03 +0200

    Add person class

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+  def initialize(age, name = 'Unknown', parent_permession = true)
+    @id = Random.rand(1..9999)
+    @name = name
+    @age = age
+    @parent_permession = parent_permession
+  end
+
+  def can_use_services?
+    is_of_age? || @parent_permession
+  end
+
+  private
+  def is_of_age?
+    @age >= 18
+  end
+end
