0deb50c38a710696c7ec8fbd1fc3869c88ade653
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-12-22 15:21:29 +0200

    Add student class as a subclass of person

---
+++ student.rb
@@ -0,0 +1,12 @@
+require "./person.rb"
+
+class Student < Person
+  def initialize(age, classroom, name = 'Unknown', parent_permession = true)
+    super(age, name, parent_permession)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    puts "¯\(ツ)/¯"
+  end
+end
