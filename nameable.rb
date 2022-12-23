8e8980ba1eb784ca003e4bee637c80043de80a6c
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-12-23 05:58:19 +0200

    Add Decorator class

--- nameable.rb
+++ nameable.rb
@@ -4,3 +4,15 @@ class Nameable
   end
 end
 
+class Decorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
