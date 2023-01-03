dc98a5c05905fd04b33e105168a27c92e93c15d9
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-01-03 18:54:05 +0200

    Fix linters

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ require './main_helper'
 class App < Arcade
   include MainHelper
 
-
-
   def create_person
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     choice = gets.chomp.to_i
--- create_person.rb
+++ create_person.rb
@@ -1,4 +1,4 @@
-#Class Create Person
+# Class Create Person
 class CreatePerson
   attr_accessor :people
 
