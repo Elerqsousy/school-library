0082077a73cf1c95df72944f641c958e2359ceed
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-01-05 17:00:27 +0200

    Fix linters

--- spec/decorators_spec.rb
+++ spec/decorators_spec.rb
@@ -7,7 +7,7 @@ describe Nameable do
   context 'When testing the Nameable class' do
     it 'The correct_name method should raise an error' do
       nameable = Nameable.new
-      expect { nameable.correct_name }.to raise_error("#{nameable.class} has not implemented method 'correct_name'", NotImplementedError)
+      expect { nameable.correct_name }.to raise_error(NotImplementedError)
     end
   end
 end
@@ -17,7 +17,7 @@ describe Decorator do
     it 'The correct_name method should raise an error' do
       nameable = Nameable.new
       decorator = Decorator.new(nameable)
-      expect { decorator.correct_name }.to raise_error("#{nameable.class} has not implemented method 'correct_name'", NotImplementedError)
+      expect { decorator.correct_name }.to raise_error(NotImplementedError)
     end
   end
 end
